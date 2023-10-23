0b2e1ec2799b672a57a9192c0b35598f23ba8712
NForlini <108763722+NForlini@users.noreply.github.com>
2023-10-23 12:46:28 +0000

    final

--- src/app.js
+++ src/app.js
@@ -4,8 +4,30 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+//import { ModuleFilenameHelpers } from "webpack";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = generateExcuse();
+  });
 };
+function generateExcuse() {
+  let who = ["I", "My Mom", "My Dad", "My Gram", "My Gramp"];
+  let action = ["broke", "lost", "stole", "ruined"];
+  let what = ["my homework", "my computer", "my keyboard", "my phone"];
+  let when = ["last night", "this morning", "earlier this week"];
+
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
+  return (
+    who[whoIndex] +
+    " " +
+    action[actionIndex] +
+    " " +
+    what[whatIndex] +
+    " " +
+    when[whenIndex]
+  );
+}
